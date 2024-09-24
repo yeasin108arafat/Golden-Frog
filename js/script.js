82f547e486601b5a1ec413ec687b6fa94f6a729e
unknown <yeasin108arafat@gmail.com>
2024-09-24 17:27:55 +0600

    About, Quality and Brand area Done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-    
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('#header').addClass('scrolled');
+        } else {
+            $('#header').removeClass('scrolled');
+        }
+    });
 });
