3088a249775daf58d8c824f29cb9baaad515ddc1
unknown <yeasin108arafat@gmail.com>
2024-09-25 10:38:03 +0600

    Contact and Footer Done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
     $(window).scroll(function() {
         if ($(this).scrollTop() > 100) {
             $('#header').addClass('scrolled');
@@ -6,4 +7,16 @@ $(document).ready(function() {
             $('#header').removeClass('scrolled');
         }
     });
+
+
+    // PARALLAX
+    const parallax = document.getElementById("parallax");
+        // Parallax Effect for DIV 1
+        window.addEventListener("scroll", function () {
+        let offset = window.pageYOffset;
+        parallax.style.backgroundPositionY = offset * 0.7 + "px";
+        // DIV 1 background will move slower than other elements on scroll.
+    });
+
+
 });
